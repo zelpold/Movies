b3246034800152c7a05a6bfaba7878cde9b48e11
zelpold <67209463+zelpold@users.noreply.github.com>
2021-12-11 16:07:49 +0300

    Добавьте файлы проекта.

---
+++ Movies/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            //return View();
+            return "This is my default action...";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
